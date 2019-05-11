f6864c1e3f008f14cf113a7d328ee5d1c7494fa6
Robert Singer <robertgsinger@gmail.com>
2019-05-11 15:51:17 -0500

    Updates readme

--- setup.py
+++ setup.py
@@ -9,24 +9,25 @@ def readme():
         return infile.read()
 
 
-classifiers=[
+classifiers = [
     # Pick your license as you wish (should match "license" above)
-    'Development Status :: 4 - Beta',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6'
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
 ]
-setup(name='django-lifecycle',
-      version='0.3.2',
-      description='Declarative model lifecycle hooks, inspired by Rails callbacks.',
-      author='Robert Singer',
-      author_email='robertgsinger@gmail.com',
-      packages=['django_lifecycle'],
-      url='https://github.com/rsinger86/django-lifecycle',
-      license='MIT',
-      keywords='django model lifecycle hooks callbacks',
-      long_description=readme(),
-      classifiers=classifiers
+setup(
+    name="django-lifecycle",
+    version="0.4.0",
+    description="Declarative model lifecycle hooks, inspired by Rails callbacks.",
+    author="Robert Singer",
+    author_email="robertgsinger@gmail.com",
+    packages=["django_lifecycle"],
+    url="https://github.com/rsinger86/django-lifecycle",
+    license="MIT",
+    keywords="django model lifecycle hooks callbacks",
+    long_description=readme(),
+    classifiers=classifiers,
 )
