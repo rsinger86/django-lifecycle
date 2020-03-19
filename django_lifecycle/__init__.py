6101d37d6926346e03c1c0186abc3e0479335591
Faisal Manzer <faisal_manzer@yahoo.in>
2020-03-19 12:50:24 +0530

    Added hooks names as variable for easy import and use

--- django_lifecycle/__init__.py
+++ django_lifecycle/__init__.py
@@ -7,6 +7,7 @@ class NotSet(object):
 
 from .decorators import hook
 from .mixins import LifecycleModelMixin
+from .hooks import *
 
 
 if IS_GTE_1_POINT_9:
---
+++ django_lifecycle/hooks.py
@@ -0,0 +1,23 @@
+BEFORE_SAVE = "before_save"
+AFTER_SAVE = "after_save"
+
+BEFORE_CREATE = "before_create"
+AFTER_CREATE = "after_create"
+
+BEFORE_UPDATE = "before_update"
+AFTER_UPDATE = "after_update"
+
+BEFORE_DELETE = "before_delete"
+AFTER_DELETE = "after_delete"
+
+
+VALID_HOOKS = (
+    BEFORE_SAVE,
+    AFTER_SAVE,
+    BEFORE_CREATE,
+    AFTER_CREATE,
+    BEFORE_UPDATE,
+    AFTER_UPDATE,
+    BEFORE_DELETE,
+    AFTER_DELETE
+)
