8d8c0aad09252aba32fada9e0e8a5806b0a30599
EnriqueSoria <ensomag@inf.upv.es>
2022-05-03 23:21:00 +0200

    refactor decorator into a dataclass

--- django_lifecycle/decorators.py
+++ django_lifecycle/decorators.py
@@ -1,5 +1,6 @@
+from dataclasses import dataclass, field
 from functools import wraps
-from typing import List, Optional
+from typing import List, Optional, Any
 
 from django_lifecycle import NotSet
 
@@ -10,66 +11,68 @@ class DjangoLifeCycleException(Exception):
     pass
 
 
-def _validate_hook_params(hook, when, when_any, has_changed, on_commit):
-    if hook not in VALID_HOOKS:
-        raise DjangoLifeCycleException(
-            "%s is not a valid hook; must be one of %s" % (hook, VALID_HOOKS)
-        )
+@dataclass
+class HookConfig:
+    hook: str
+    when: Optional[str] = None
+    when_any: Optional[List[str]] = None
+    was: Any = "*"
+    is_now: Any = "*"
+    has_changed: Optional[bool] = None
+    is_not: Any = NotSet
+    was_not: Any = NotSet
+    changes_to: Any = NotSet
+    on_commit: bool = False
 
-    if has_changed is not None and not isinstance(has_changed, bool):
-        raise DjangoLifeCycleException("'has_changed' hook param must be a boolean")
-
-    if when is not None and not isinstance(when, str):
-        raise DjangoLifeCycleException(
-            "'when' hook param must be a string matching the name of a model field"
-        )
-
-    if when_any is not None:
-        when_any_error_msg = (
-            "'when_any' hook param must be a list of strings "
-            "matching the names of model fields"
-        )
-
-        if not isinstance(when_any, list):
-            raise DjangoLifeCycleException(when_any_error_msg)
-
-        if len(when_any) == 0:
+    def __post_init__(self):
+        if self.hook not in VALID_HOOKS:
             raise DjangoLifeCycleException(
-                "'when_any' hook param must contain at least one field name"
+                "%s is not a valid hook; must be one of %s" % (hook, VALID_HOOKS)
             )
 
-        for field_name in when_any:
-            if not isinstance(field_name, str):
+        if self.has_changed is not None and not isinstance(self.has_changed, bool):
+            raise DjangoLifeCycleException("'has_changed' hook param must be a boolean")
+
+        if self.when is not None and not isinstance(self.when, str):
+            raise DjangoLifeCycleException(
+                "'when' hook param must be a string matching the name of a model field"
+            )
+
+        if self.when_any is not None:
+            when_any_error_msg = (
+                "'when_any' hook param must be a list of strings "
+                "matching the names of model fields"
+            )
+
+            if not isinstance(self.when_any, list):
                 raise DjangoLifeCycleException(when_any_error_msg)
 
-    if when is not None and when_any is not None:
-        raise DjangoLifeCycleException(
-            "Can pass either 'when' or 'when_any' but not both"
-        )
-    
-    if on_commit is not None:
-        if not hook.startswith("after_"):
-            raise DjangoLifeCycleException("'on_commit' hook param is only valid with AFTER_* hooks")
+            if len(self.when_any) == 0:
+                raise DjangoLifeCycleException(
+                    "'when_any' hook param must contain at least one field name"
+                )
 
-        if not isinstance(on_commit, bool):
-            raise DjangoLifeCycleException("'on_commit' hook param must be a boolean")
+            for field_name in self.when_any:
+                if not isinstance(field_name, str):
+                    raise DjangoLifeCycleException(when_any_error_msg)
 
+        if self.when is not None and self.when_any is not None:
+            raise DjangoLifeCycleException(
+                "Can pass either 'when' or 'when_any' but not both"
+            )
 
-def hook(
-    hook: str,
-    when: str = None,
-    when_any: List[str] = None,
-    was="*",
-    is_now="*",
-    has_changed: bool = None,
-    is_not=NotSet,
-    was_not=NotSet,
-    changes_to=NotSet,
-    on_commit: Optional[bool] = None
-):
-    _validate_hook_params(hook, when, when_any, has_changed, on_commit)
+        if self.on_commit:
+            if not self.hook.startswith("after_"):
+                raise DjangoLifeCycleException(
+                    "'on_commit' hook param is only valid with AFTER_* hooks"
+                )
 
-    def decorator(hooked_method):
+            if not isinstance(self.on_commit, bool):
+                raise DjangoLifeCycleException(
+                    "'on_commit' hook param must be a boolean"
+                )
+
+    def __call__(self, hooked_method):
         if not hasattr(hooked_method, "_hooked"):
 
             @wraps(hooked_method)
@@ -80,21 +83,9 @@ def hook(
         else:
             func = hooked_method
 
-        func._hooked.append(
-            {
-                "hook": hook,
-                "when": when,
-                "when_any": when_any,
-                "has_changed": has_changed,
-                "is_now": is_now,
-                "is_not": is_not,
-                "was": was,
-                "was_not": was_not,
-                "changes_to": changes_to,
-                "on_commit": on_commit
-            }
-        )
+        func._hooked.append(self)
 
         return func
 
-    return decorator
+
+hook = HookConfig  # keep backwards compatibility
