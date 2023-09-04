1403c2374a43a8fe222fdc62df1ac7331643e04b
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2023-09-04 15:15:01 +0800

    🚧chore: Add support for Django 4.2
    fixed #114
    
    adopted some of the changes in #115
    
    and those in https://github.com/rsinger86/django-lifecycle/compare/0.9.2...0.9.3
    
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ classifiers = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Framework :: Django",
     "Framework :: Django :: 2.0",
     "Framework :: Django :: 2.1",
@@ -37,6 +38,9 @@ classifiers = [
     "Framework :: Django :: 3.0",
     "Framework :: Django :: 3.1",
     "Framework :: Django :: 3.2",
+    "Framework :: Django :: 4.0",
+    "Framework :: Django :: 4.1",
+    "Framework :: Django :: 4.2",
 ]
 setup(
     name="django-lifecycle",
